b8fa91b09d2fc4c88a1197d44b80bc41d3904c4b
Kan Alexandr <kan.a@delotech.ru>
2026-06-26 12:48:01 +0300

    edited config

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str
     model_config = SettingsConfigDict(extra="ignore", env_file=".env")
-    api_key: str = "secret-key"
+    api_key: str = "super-secret-key"
 
 
 settings = Settings()
