a355e1c4af699073b644e386927755bde5a33f77
Kan Alexandr <kan.a@delotech.ru>
2026-06-26 13:37:47 +0300

    initialized rabbitmq objects

--- app/config.py
+++ app/config.py
@@ -3,8 +3,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
-    model_config = SettingsConfigDict(extra="ignore", env_file=".env")
+    rabbitmq_url: str
     api_key: str = "super-secret-key"
+
+    model_config = SettingsConfigDict(extra="ignore", env_file=".env")
 
 
 settings = Settings()
---
+++ app/worker/broker.py
@@ -0,0 +1,16 @@
+from faststream.rabbit import RabbitBroker, RabbitQueue, RabbitExchange, ExchangeType
+from app.config import settings
+
+broker = RabbitBroker(settings.rabbitmq_url)
+
+dead_letter_exchange = RabbitExchange("payments.dlx", type=ExchangeType.FANOUT, durable=True)
+
+payments_new = RabbitQueue("payments.new", durable=True, arguments={"x-dead-letter-exchange": "payments.dlx"}, )
+
+payments_dlq = RabbitQueue("payments.dlq", durable=True)
+
+
+async def declare_dlq() -> None:
+    exchange = await broker.declare_exchange(dead_letter_exchange)
+    queue = await broker.declare_queue(payments_dlq)
+    await queue.bind(exchange)
