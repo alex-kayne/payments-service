17f07efec539b530b890033626e178004e1aa1dd
Kan Alexandr <kan.a@delotech.ru>
2026-06-26 12:20:30 +0300

    edited PaymentService

--- app/service.py
+++ app/service.py
@@ -12,23 +12,31 @@ class PaymentService:
         self.payment_repo = payment_repo
         self.outbox_repo = outbox_repo
 
-    async def create_payment(self, data: PaymentCreate, idempotency_key: str) -> Payment:
+    async def get_payment(self, payment_id: int) -> Payment | None:
         async with async_session_maker() as async_session:
-            if existing := await self.payment_repo.get_by_idempotency_key(async_session, idempotency_key):
-                return existing
-            payment = Payment(
-                amount=data.amount,
-                currency=data.currency,
-                description=data.description,
-                meta=data.metadata,
-                idempotency_key=idempotency_key,
-                webhook_url=data.webhook_url, )
-            await self.payment_repo.create_payment(async_session, payment)
+            return await self.payment_repo.get_by_id(async_session, payment_id)
 
-            event = Outbox(aggregate_id=payment.id,
-                           event_type="payment.created",
-                           payload={"payment_id": payment.id}, )
+    async def create_payment(self, data: PaymentCreate, idempotency_key: str) -> Payment | None:
+        try:
+            async with async_session_maker().begin() as async_session:
+                if existing := await self.payment_repo.get_by_idempotency_key(async_session, idempotency_key):
+                    return existing
+                payment = Payment(
+                    amount=data.amount,
+                    currency=data.currency,
+                    description=data.description,
+                    meta=data.metadata,
+                    idempotency_key=idempotency_key,
+                    webhook_url=data.webhook_url, )
+                await self.payment_repo.create_payment(async_session, payment)
 
-            await self.outbox_repo.create_outbox(async_session, event)
+                event = Outbox(aggregate_id=payment.id,
+                               event_type="payment.created",
+                               payload={"payment_id": payment.id}, )
 
-            return payment
+                await self.outbox_repo.create_outbox(async_session, event)
+
+                return payment
+        except IntegrityError:
+            async with async_session_maker() as async_session:
+                return await self.payment_repo.get_by_idempotency_key(async_session, idempotency_key)
