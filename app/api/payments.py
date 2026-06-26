cdef0f25c9875bd2657f5c6f90e108973f1dba26
Kan Alexandr <kan.a@delotech.ru>
2026-06-26 12:45:02 +0300

    edited api layer

--- app/api/payments.py
+++ app/api/payments.py
@@ -1,29 +1,30 @@
 from fastapi import APIRouter, Depends, status, Header, HTTPException
 
-from app.api import deps
+from app.api.deps import get_payment_service
 from app.schemas import PaymentCreateResponse, PaymentCreate, PaymentRead
+from app.security import verify_api_key
 from app.service import PaymentService
 
 router = APIRouter(
     prefix="/api/v1/payments",
     tags=["payments"],
-    dependencies=[Depends(deps.get_payment_service)]
+    dependencies=[Depends(verify_api_key)]
 )
 
 
-@router.post("", status_code=status.HTTP_202_CREATED, response_model=PaymentCreateResponse)
+@router.post("", status_code=status.HTTP_202_ACCEPTED, response_model=PaymentCreateResponse)
 async def create_payment(
         data: PaymentCreate,
         idempotency_key: str = Header(..., alias="Idempotency-Key"),
-        service: PaymentService = Depends(deps.get_payment_service),
+        service: PaymentService = Depends(get_payment_service),
 ):
-    return service.create_payment(data, idempotency_key)
+    return await service.create_payment(data, idempotency_key)
 
 
 @router.get("/{payment_id}", response_model=PaymentRead)
 async def get_payment(
         payment_id: int,
-        service: PaymentService = Depends(deps.get_payment_service),
+        service: PaymentService = Depends(get_payment_service),
 ):
     payment = await service.get_payment(payment_id)
     if payment is None:
